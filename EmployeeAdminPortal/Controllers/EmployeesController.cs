5f648cf4fcbfd79179287399187569da9ef691ef
themilnera <amilner32@gmail.com>
2025-07-18 13:12:44 -0600

    Complete basic CRUD operations: Get all route, Get by id, Add, Update and Delete routes.

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,4 +1,6 @@
 ﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,10 +24,64 @@ namespace EmployeeAdminPortal.Controllers
             return Ok(dbContext.Employees.ToList()); //200 success OK response
         }
 
-        [HttpPost]
-        public IActionResult AddEmployee()
+        [HttpGet]
+        [Route("{id:guid}")] //match the name here 'id' to the param
+        public IActionResult GetEmployeeById(Guid id) 
         {
+            var employee = dbContext.Employees.Find(id);
+            if(employee is null)
+            {
+                return NotFound();
+            }
 
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+            return Ok(employeeEntity);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if(employee is null)
+            {
+                return NotFound();
+            }
+            employee.Name = updateEmployeeDto.Name; 
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            dbContext.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            dbContext.Remove(employee);
+            dbContext.SaveChanges();
+            return Ok();
         }
     }
 }
