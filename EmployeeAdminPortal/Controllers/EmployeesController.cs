2b0e8e87b2acdb2be5f95c8c04781c4a9329ec40
themilnera <amilner32@gmail.com>
2025-07-18 12:47:02 -0600

    Add project files.

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    // localhost:xxxx/api/[controller]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(dbContext.Employees.ToList()); //200 success OK response
+        }
+
+        //[HttpPost]
+        //public IActionResult AddEmployee()
+        //{
+
+        //}
+    }
+}
